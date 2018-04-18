23242533f364ab7fbcbbb1eec92bc877a9a979f6
csevere <carla.severe@gmail.com>
2018-04-18 02:17:26 -0400

    working on errors

---
+++ config/config.js
@@ -0,0 +1,9 @@
+const config = {
+  host: 'us-cdbr-iron-east-05.cleardb.net',
+  user: 'b78f6fcfb18642',
+  password:'460878de',
+  database: 'heroku_9247d7bae2069c2' 
+}
+
+module.exports = config; 
+
