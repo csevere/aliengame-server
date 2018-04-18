93e77bf0d44aa36153494787ce2e63fa21b0e360
csevere <carla.severe@gmail.com>
2018-04-18 16:15:38 -0400

    working on db connection

--- routes/stats.js
+++ routes/stats.js
@@ -11,8 +11,11 @@ module.exports = function(router){
     //check the char
     var selectCharName = "SELECT * FROM `characters` WHERE `character` = ?;";
     connection.query(selectCharName, [charData.character], (error, results)=>{
+      //done with the connection
+      
       console.log('**********CHECKING CHAR RESULTS*********') 
       console.log(results); 
+
       if(error) throw error;
       if(results.length > 0){
         const updateStatsQuery = "UPDATE `characters` SET level = ?, experience = ?, time = ?  WHERE `character` = ?;"; 
@@ -23,6 +26,7 @@ module.exports = function(router){
           }
           console.log(results); 
           console.log("stats updated success!")
+          connection.end();
         }); 
 
         const checkCharName2 = "SELECT * FROM `characters` WHERE `character` = ?;";
